8e0cce2f4502126fd0146505c6c96db58b5f27df
prabhuramkumar <prabhu.ramkumar1@gmail.com>
2015-11-17 12:13:28 +0530

    Prabhu - New project files

---
+++ README.md
@@ -0,0 +1,33 @@
+# react-webpack-babel
+Simple React Webpack Babel Starter Kit
+
+
+This is a simple [React](https://facebook.github.io/react/), [Webpack](http://webpack.github.io/) and [Babel](https://babeljs.io/) application with nothing else in it.
+
+### What's in it?
+
+Just a simple [index.jsx](./index.jsx), [webpack.config.js](./webpack.config.js) and [index.html](./public/index.html) file.
+
+### To run
+
+Install webpack and the development server:
+
+```
+> $ npm i webpack-dev-server webpack -g
+```
+
+You can simply run webpack build using this command: 
+
+```
+> $ npm run build
+```
+
+If you want to run with webpack-dev-server simply run this command: 
+
+```
+> $ npm run dev
+```
+
+Open the web browser to `http://localhost:8080/`
+
+Please contribute to the project if you think this can be done better in anyway even for this README :)
---
+++ commentBox.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import marked from 'marked';
+import jquery from 'jquery';
+var $ = jquery;
+
+var CommentBox = React.createClass({
+	getInitialState: function() {
+		return {data: []};
+	},
+	loadCommentsFromServer: function (){
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(serverData){
+				this.setState({data: serverData});
+			}.bind(this),
+			error: function(){
+				console.error(this.props.url, status);
+			}.bind(this)
+		})
+	},
+
+	updateNewComment: function(comment){
+		//Post the comment on UI before updating the JSON
+		var comments = this.state.data;
+		comment.id = Date.now();
+		var newComments = comments.concat([comment]);
+		this.setState({data: newComments});
+
+		$.ajax({
+		  url: this.props.url,
+		  dataType: 'json',
+		  type: 'POST',
+		  data: comment,
+		  success: function(data) {
+		    this.setState({data: data});
+		  }.bind(this),
+		  error: function(xhr, status, err) {
+		    console.error(this.props.url, status, err.toString());
+		  }.bind(this)
+		});
+	},
+
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		//setInterval(this.loadCommentsFromServer, this.props.interval);
+	},
+
+	render: function(){
+		return (
+			<div className="commentBox">
+				<CommentList data={this.state.data} />
+				<CommentForm onPostComment={this.updateNewComment} />
+			</div>
+		);
+	}
+});
+
+var CommentList = React.createClass({
+	render: function(){
+		var commentNodes = this.props.data.map(function(comment){
+			return (
+				<Comment author={comment.author} key={comment.id}>{comment.text}</Comment>
+			);
+		});
+		return (
+			<div className="commentList">
+				{commentNodes}
+			</div>
+		);
+	}
+});
+
+
+var CommentForm = React.createClass({
+	handleSubmit: function(e){
+		e.preventDefault();
+		console.log(this.refs);
+		var author = this.refs.author.value.trim();
+		var text = this.refs.text.value.trim();
+
+		if(!text || !author){
+			alert("submit some text");
+			return; 
+		}
+
+		this.props.onPostComment({author: author, text: text});
+		this.refs.author.value = '';
+		this.refs.text.value = '';
+	},
+
+	render: function(){
+		return(
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+		        <input type="text" placeholder="Your name" ref="author" />
+		        <br></br>
+		        <textarea placeholder="Say something..." ref="text" />
+		        <br></br>
+		        <input type="submit" value="Post" />
+		    </form> 
+	    )
+	}
+});
+
+var Comment = React.createClass({
+	rawMarkup: function(){
+		var rawMarkup = marked(this.props.children.toString(), {santize: true});
+		return {__html: rawMarkup};
+	},
+	render: function(){
+		return (
+			<div className="comment">
+				<h3 className="author">
+					{this.props.author}
+				</h3>
+				<span dangerouslySetInnerHTML={this.rawMarkup()}></span>
+			</div>
+		);
+	}
+});
+
+
+module.exports = CommentBox;
+
