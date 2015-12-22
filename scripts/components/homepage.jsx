ea2b34e2c9c4726d622c5987a10eb132e5d3d463
prabhuramkumar <prabhu.ramkumar1@gmail.com>
2015-12-22 10:24:54 +0530

    Prabhu - adding link instead of anchor tag

--- scripts/components/homepage.jsx
+++ scripts/components/homepage.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import { Router, Route, IndexRoute, Link, IndexLink } from 'react-router';
 
 var HomePage = React.createClass({
 	render: function(){
 		return(
 			<div className="home-page">
-				<div className="welcome">
+				<section className="welcome">
 					<h1> Hi, Thola!!</h1>
 					<h3>We welcome you here!!</h3>
-				</div>
-				<div className="intro-block">
+				</section>
+				<section className="intro-block">
 					<div className="intro">
 						<h4>Own a car?</h4>
 						<p>Offer a ride to your collegues by just posting your regular commute, we will connect you with people on your route.</p>
-						<a href="/create" className="btn-primary btn">Offer a Ride</a>
+						<Link      to="/create" className="btn-primary btn">Search</Link>
 					</div>
 					<div className="intro">
 						<h4>Dont own a car?</h4>
 						<p>Take a ride with your collegue by just posting your regular commute, we will connect you with people on your route.</p>
-						<a href="/search" className="btn-primary btn">Take a Ride</a>
+						<Link to="/search" className="btn-primary btn">Search</Link>
 					</div>
-				</div>
+				</section>
 				<h4 className="purpose">Share or Offer a ride to people and contribute to the traffic solution to your city through this small intiative. </h4>
 			</div>
 
